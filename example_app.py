44ff75e85ee4c33080ceb646c15ed4cbe0117ccc
FowluhhDev <trfowluhh@gmail.com>
2024-10-19 21:16:23 -0400

    cooleyer

--- example_app.py
+++ example_app.py
@@ -1,4 +1,4 @@
-from fowl import init, input, draw
+from fowl import init, input, draw, object
 from fowl.scene import scene, manager
 
 # Constants
@@ -6,20 +6,28 @@ WIDTH = 640
 HEIGHT = 480
 FPS = 60
 
+class LogoObject(object.Object):
+    def __init__(self):
+        self.image = draw.get_image("assets/image.png")
+        self.imagesize = 0.5
+        self.texture = draw.get_texture(self.image)
+    
+    def draw(self):
+        draw.draw_texture(draw.find_center(WIDTH, self.image.width*self.imagesize), draw.find_center(HEIGHT, self.image.height*self.imagesize) - 32, self.texture, self.imagesize)
+        return super().draw()
+
 # Classes
 class TestScene(scene.Scene):
     def init(self):
         print("Hello, TestScene!")
-        self.image = draw.get_image("image.png")
-        self.imagesize = 0.5
-        self.texture = draw.get_texture(self.image)
         self.text = "Hello, FowlEngine!"
         self.textsize = 32
         self.font = draw.get_font("assets/font.fnt")
+        self.logo = LogoObject()
         return super().init()
     
     def draw(self):
-        draw.draw_texture(draw.find_center(WIDTH, self.image.width*self.imagesize), draw.find_center(HEIGHT, self.image.height*self.imagesize) - 32, self.texture, self.imagesize)
+        self.logo.draw()
         draw.draw_text(draw.find_text_center(self.font, self.text, self.textsize), draw.find_center(HEIGHT, 24)+48, self.text, self.font, self.textsize)
         return super().draw()
     
---
+++ fowl/object.py
@@ -0,0 +1,9 @@
+class Object:
+    def __init__(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def update(self):
+        pass
